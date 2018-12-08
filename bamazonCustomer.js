9838cadabcc46209dd90e25d10639f5145a4ee8f
kallistahalim <kallistahalim@gmail.com>
2018-12-08 10:10:42 -0800

    added if else statements

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,29 +1,54 @@
 var mysql = require("mysql");
+var inquirer = require("inquirer");
 
 var connection = mysql.createConnection({
-  host: "localhost",
+    host: "localhost",
 
-  // Your port; if not 3306
-  port: 3306,
+    // Your port; if not 3306
+    port: 3306,
 
-  // Your username
-  user: "root",
+    // Your username
+    user: "root",
 
-  // Your password
-  password: "Moderndance123",
-  database: "bamazon"
+    // Your password
+    password: "Moderndance123",
+    database: "bamazon"
 });
 
-connection.connect(function(err) {
-  if (err) throw err;
-  console.log("connected as id " + connection.threadId);
-  afterConnection();
+connection.connect(function (err) {
+    if (err) throw err;
+    console.log("connected as id " + connection.threadId);
+    afterConnection();
 });
 
 function afterConnection() {
-  connection.query("SELECT * FROM products", function(err, res) {
-    if (err) throw err;
-    console.log(res);
-    connection.end();
-  });
-}
+    connection.query("SELECT * FROM products", function (err, res) {
+        if (err) throw err;
+
+
+        else {
+
+            inquirer.prompt([{
+                name: "ID",
+                message: "What is your product ID number?"
+            }, {
+                name: "units",
+                message: "What quantity do you wish to order?"
+            }]).then(function (answer) {
+                    for (var i = 0; i < res.length; i++) {
+                        // console.log(res[i].item_id);
+                        console.log(answer.ID);
+                        // if (answer.ID === res[i].item_id) {
+                        //     console.log("yay")
+                        // }
+
+                    }
+                    })
+
+
+
+            };
+
+            connection.end();
+        })
+    };
